bff57b8d367764fcf80ed114f2c302f89fabd466
Roger Niederhauser <roger.niederhauser@siemens.com>
2020-03-21 10:42:41 +0100

    feat: add todo list

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>{{ todo.text }}</li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+export interface Todo {
+  id: number;
+  text: string;
+}
+
+@Component
+export default class TodoItem extends Vue {
+  @Prop() private todo!: Todo;
+}
+</script>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-list">
+    <h1>Todo List</h1>
+    <TodoItem v-for="todo in groceryList" :todo="todo" :key="todo.id" />
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      groceryList: [
+        { id: 1, text: "Buy apples" },
+        { id: 2, text: "Buy pears" },
+        { id: 3, text: "What do you humans need" }
+      ]
+    };
+  }
+};
+</script>
